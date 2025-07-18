4721c3b4d6cf53865644c5be25b737bdf3260d1b
Vivek-Actowiz <vivekpankhaniyaactowiz@gmail.com>
2025-07-18 17:18:57 +0530

    add cors

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,27 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const authRoutes = require("./routes/AuthRoutes");
 const connectDB = require("./config/db");
 
 dotenv.config();
 
 const app = express();
+const FRONTEND_URL = process.env.FRONTEND_URL;
+app.use(
+  cors({
+    origin: { FRONTEND_URL },
+  })
+);
 app.use(express.json());
 
 connectDB(); // DB connect
 
 app.use("/api", authRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+app.listen(process.env.PORT, "0.0.0.0", () => {
+  console.log(
+    `Server running on port http://0.0.0.0:${process.env.PORT} or http://localhost:${process.env.PORT}`
+  );
 });
