c76ed279836db97e0c9919b6edba42b24382400f
Vivek-Actowiz <vivekpankhaniyaactowiz@gmail.com>
2025-07-18 17:13:34 +0530

    add cors

--- backend/models/AdminModel.js
+++ backend/models/AdminModel.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema(
+const adminSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
@@ -11,4 +11,4 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("Admin", adminSchema);
--- backend/routes/AuthRoutes.js
+++ backend/routes/AuthRoutes.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { signup, login } = require("../controllers/AuthController");
+const {
+  signup,
+  login,
+  Adminsignup,
+  Adminlogin,
+} = require("../controllers/AuthController");
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.post("/Adminsignup", Adminsignup);
+router.post("/Adminlogin", Adminlogin);
 
 module.exports = router;
